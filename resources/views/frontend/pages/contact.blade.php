3205af7d6b33def74b3b9873ef5f5de2d10b0e54
Naman <07032001nk@gmail.com>
2021-08-05 19:17:18 +0530

    Added more stuff

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -13,6 +13,14 @@
 
 <br>
 
+<div class="m-2">
+<div class="m-auto w-4/5 bg-white border-2 rounded p-4">
 
+        <h1 class="text-3xl font-semibold">
+                If you want to grow your business through online Ads, you can either call or, or fill the form so that we can call you
+        </h1>
+
+</div>
+</div>
 
 @endsection
